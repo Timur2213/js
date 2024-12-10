7c71904391ce016fe00a1b76a21ee9ff33c28245
Timur2213 <suslovtimur91@gmail.com>
2024-12-10 18:55:30 +0400

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var body = document.getElementById('body')
+
+
+
+function start() {
+
+    //alert('Hello')
+    console.dir(body.style)
+    //body.style.backgroundColor='black'
+
+
+
+    if (body.style.backgroundColor == 'black' & body.style.textAlign=='center') 
+        {body.style.backgroundColor == 'white' & body.style.textAlign=='left'}
+    else {body.style.backgroundColor = 'black' &  body.style.textAlign='center'}
+   
+    
+ 
+    
+
+ }
